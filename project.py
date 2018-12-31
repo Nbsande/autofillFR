3bec478c0896f97a485cf4e3b073ce5caa6245ad
Neil B <neil.botelho321@gmail.com>
2018-12-31 18:27:07 +0530

    Selenium comments in test and added a while loop to deal with non y or n answers

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 #setup
 
-from cv2 import selectROI, imread, imwrite, IMREAD_GRAYSCALE
+from cv2 import selectROI, imread, imwrite, IMREAD_GRAYSCALE, destroyAllWindows
 from numpy import asarray
 from pytesseract import image_to_string
 from os import listdir ,path
@@ -43,8 +43,6 @@ def ROIselector(picture,item):
         showCrosshair = False
         fromCenter = False
         r=(0,0,0,0)
-        print(picture)
-        print(item)
         while r==(0,0,0,0):
             r=selectROI(item,picture, fromCenter, showCrosshair)
             if r==(0,0,0,0):
@@ -53,32 +51,40 @@ def ROIselector(picture,item):
                 skipQuestion=messagebox.askquestion("Retry?","Press Yes to retry selection or press no to skip this image.")
                 sleep(1)
                 if skipQuestion=='no':
+                    global skipVariable
                     skipVariable=1
                     assert skipVariable==0
-            
+        destroyAllWindows()
+        imCrop = picture[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+        # Display cropped image
+    return imCrop, skipVariable   
     
 def Tesseract(img):
     return image_to_string(img)
 
+
 def preProcess(img_path='/home/neil/Desktop/a/grayimage.jpg',foldername="/home/neil/wordpress/output/"):
     img=imread(img_path,IMREAD_GRAYSCALE)
     img[img[:]<100]=asarray(0)
+    
     img[img[:]!=0]=asarray(255)
     return img
 
 def errorRecursion(fields_to_collect,img,fieldStore,img_path):
-    print(img)
-    vError=0
-    while vError==0:
+    vError=1
+    while vError==1:
         vError=0
         for item in fields_to_collect:
                     try:
-                        imageText=Tesseract(ROIselector(img,item))
+                        image,skipVariable=ROIselector(img,item)
+                        imageText=Tesseract(image)
+                        sleep(1)
                         print("Is the following correct:")
                         print(item+": "+imageText)
                         confirmation=input()
                         if confirmation=='y'or confirmation=='Y':
                             fieldStore[item]=imageText
+                            sleep(1)
                         else: 
                             if confirmation=='n' or confirmation=='N':
                                 print('Enter correct value of field')
@@ -95,6 +101,7 @@ def errorRecursion(fields_to_collect,img,fieldStore,img_path):
                         messagebox.showwarning('Something other than y or n was input','Type only y or n in the input box')
                         print('Try again.')
                         vError=1
+    return skipVariable
 
 def skip_folder_gen(folder):
     try:
@@ -104,26 +111,12 @@ def skip_folder_gen(folder):
         print('made directory')
     except FileExistsError:
         pass
-    process=check_output(['mkdir','skipped'],shell=True)
     for file_path in skip:
         file_name=file_path.split('/')
         copy(file_name[-1],folder+'skipped/'+file_name[-1])
 
 
 def MainProcess():
-    V1error=0
-    while V1error==1:
-        try:
-            print('Do you want to review the information collected after each file is finished?(y/n)')
-            reviewAfterEveryFile=input()
-            if reviewAfterEveryFile not in ['y','n']:
-                raise ValueError
-        except ValueError:
-            root=Tk()
-            root.withdraw()
-            messagebox.showwarning('ValueError','Type only y or n as input box')
-            print('Try again.')
-            vError=1
     root = Tk()
     root.withdraw()
     root.filename =  filedialog.askdirectory(parent=root,initialdir="/home/neil/Desktop",title='Please select a directory')   
@@ -132,28 +125,46 @@ def MainProcess():
     imgs=[img for img in folder_all if img[-3:]=='jpg']
     img_paths=[join(root.filename,img) for img in imgs]
     for img_path in img_paths:
+        
         img=preProcess(img_path)
         fieldStore={}
-        errorRecursion(fields_to_collect,img,fieldStore,img_path)
+        skipVariable= errorRecursion(fields_to_collect,img,fieldStore,img_path)
         intermediate_field_store={img_path:fieldStore}
         if skipVariable==0:
-            if reviewAfterEveryFile=='y':
-                print('The data collected so far is:')
-                for field in fields_to_collect:
-                    print(field+": "+str(intermediate_field_store[img_path][field]))
-                confirmation=input('Is the data correct?(y/n)')
-                if confirmation=='y':
-                    submissions.update(intermediate_field_store)
-                    intermediate_field_store={}
-                else:
-                    if confirmation=='n':
-                        intermediate_field_store={}
-                        skip.append(img_path)
-                    else:
-                        raise Exception('Something other than y or n was input')
-            else:
+            print('The data collected so far is:')
+            for field in fields_to_collect:
+                print(field+": "+str(intermediate_field_store[img_path][field]))
+            confirmation=input('Is the data correct?(y/n)')
+            if confirmation=='y':
                 submissions.update(intermediate_field_store)
                 intermediate_field_store={}
+            else:
+                if confirmation=='n':
+                    root.withdraw()
+                    messagebox.showwarning('IncorrectDataWarning','Since the collected information is wrong this image will be skipped and added to the skip folder')
+                    intermediate_field_store={}
+                    skip.append(img_path)
+                else:
+                    wrongInputVar=1
+                    while wrongInputVar==1:
+                        print('Something other than \'y\' or \'n\' was typed. Try again with only \'y\' or \'n\'.')
+                        confirmation=input('Is the data correct?(y/n)')
+                        if confirmation=='y':
+                            submissions.update(intermediate_field_store)
+                            intermediate_field_store={}
+                            wrongInputVar=0
+                        else:
+                            if confirmation=='n':
+                                root.withdraw()
+                                messagebox.showwarning('IncorrectDataWarning','Since the collected information is wrong this image will be skipped and added to the skip folder')
+                                intermediate_field_store={}
+                                skip.append(img_path)
+                                wrongInputVar=0
+                            else:
+                                print('try again with either y or n')
+        
+            submissions.update(intermediate_field_store)
+            intermediate_field_store={}
         else:
             intermediate_field_store={}
             skipVariable=0 
@@ -162,6 +173,7 @@ def MainProcess():
 
 
 n=""
+
 while n!= "exit":
     print(">>>",end="")
     n=input()
--- test.py
+++ test.py
@@ -2,30 +2,65 @@
 with open('fileStore.py','w') as f:
     f.write("fileStore={}".format(str(di)))
     f.close()"""
-"""
+
 import pyautogui as p
-p.FAILSAFE=False
-p.click(583,101)
-p.hotkey('ctrlleft','a')
-p.typewrite('hello')
-for n in range(10):
+import time
+#p.FAILSAFE=False
+#p.click(583,101)
+#p.hotkey('ctrlleft','a')
+#browser=webdriver.chromedriver()
+Surname=(488,578)#browser.find_element_by_name('applicant_surname')
+Given_name=(496,600)#browser.find_element_by_name('applicant_givenname')
+Sex_select=(517,618)#from selenium.webdriver.support.ui import Select# Select(driver.find_element_by_name('applicant_sex')).select_by_visible_text('Male/Female/Transgender')
+Sex_male=(504,647)
+Sex_female=(504,662)
+Sex_transgender=(504,676)
+DOB=(515,676)
+Age=(492,734)
+Real_dob=(767,733)
+Scroll='p.scroll(-11)'
+Specialcategory_select=(507,183)
+Specialcategories={}
+#p.typewrite('hello')
+#p.moveTo(932,124)
+time.sleep(3)
+p.scroll(-15)
+#p.displayMousePosition()
+#print(p.position())
+#p.moveTo(Surname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''for n in range(10):
     p.click(683,100+n*20)
     p.typewrite("it worked!!")
     p.typewrite('\n')
     #p.hotkey('ctrlleft','v')
 p.moveTo(0,0,3)
-p.moveTo(500,500)"""
-"""from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+p.moveTo(500,500)'''
+"""
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
 import pyautogui as p
-driver=webdriver.Chrome(executable_path='/home/neil/Desktop/chromedriver')
-driver.get('http:/python.org')
-assert 'Python' in driver.title
+#driver=webdriver.Chrome(executable_path='/home/neil/Desktop/chromedriver')
+#driver.get('http:/python.org')
+#assert 'Login Page' in driver.title
 print(driver.title)
-p.click(808,264)
-p.typewrite('windows\n')
-driver.close()"""
 
+#p.typewrite('windows\n')
+#driver.close()
+#""""""
 from pytesseract import image_to_string
 print(image_to_string('/home/neil/wordpress/output/output.jpg'))
     #for i in range(3):
@@ -36,4 +71,4 @@ print(image_to_string('/home/neil/wordpress/output/output.jpg'))
     #scipy.misc.imsave(arr,str(foldername)+"output.jpg")
    
     #img=imread(str(foldername)+"output.jpg")
-    #return img
+    #return img"""
